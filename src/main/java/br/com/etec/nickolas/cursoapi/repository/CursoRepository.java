b9b83280413722108e7cdd1a3abee5e7dbb8c17e
MelloNickolas <nickolasphmello10@gmail.com>
2023-05-04 08:54:29 -0300

    Filter Started

--- src/main/java/br/com/etec/nickolas/cursoapi/repository/CursoRepository.java
+++ src/main/java/br/com/etec/nickolas/cursoapi/repository/CursoRepository.java
@@ -1,9 +1,10 @@
 package br.com.etec.nickolas.cursoapi.repository;
 
 import br.com.etec.nickolas.cursoapi.model.Curso;
+import br.com.etec.nickolas.cursoapi.repository.curso.CursoRepositoryQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CursoRepository extends JpaRepository<Curso, Integer> {
+public interface CursoRepository extends JpaRepository<Curso, Integer>, CursoRepositoryQuery {
 }
