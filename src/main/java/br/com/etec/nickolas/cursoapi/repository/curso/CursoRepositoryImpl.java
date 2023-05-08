200ac51629636e5deb3ed8c9d44097bfea3a04c1
MelloNickolas <nickolasphmello10@gmail.com>
2023-05-08 07:51:32 -0300

    CursoRepository Initialized

--- src/main/java/br/com/etec/nickolas/cursoapi/repository/curso/CursoRepositoryImpl.java
+++ src/main/java/br/com/etec/nickolas/cursoapi/repository/curso/CursoRepositoryImpl.java
@@ -4,7 +4,7 @@ import br.com.etec.nickolas.cursoapi.model.Curso;
 import br.com.etec.nickolas.cursoapi.repository.filter.CursoFilter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
+//
 public class CursoRepositoryImpl implements CursoRepositoryQuery{
     @Override
     public Page<Curso> Filtrar(CursoFilter cursoFilter, Pageable pageable) {
