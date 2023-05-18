de1df92fdd1d7f0f9b5cc62055df0c6fa1095abc
MelloNickolas <nickolasphmello10@gmail.com>
2023-05-18 08:56:49 -0300

    Order By processing...

--- src/main/java/br/com/etec/nickolas/cursoapi/repository/curso/CursoRepositoryImpl.java
+++ src/main/java/br/com/etec/nickolas/cursoapi/repository/curso/CursoRepositoryImpl.java
@@ -2,18 +2,21 @@ package br.com.etec.nickolas.cursoapi.repository.curso;
 
 import br.com.etec.nickolas.cursoapi.model.Curso;
 import br.com.etec.nickolas.cursoapi.repository.filter.CursoFilter;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.util.Predicates;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class CursoRepositoryImpl implements CursoRepositoryQuery{
 
@@ -28,15 +31,24 @@ public class CursoRepositoryImpl implements CursoRepositoryQuery{
         Root<Curso> root = criteria.from(Curso.class);
 
         Predicate[] predicates = criarRestricoes(cursoFilter, builder, root);
-
         criteria.where(predicates);
+        criteria.orderBy(builder.asc(root.get("nomecurso")));
+
+        TypedQuery<Curso> query = manager.createQuery(criteria);
 
         return null;
     }
 
     private Predicate[] criarRestricoes(CursoFilter cursoFilter, CriteriaBuilder builder, Root<Curso> root) {
 
-        List<Predicates> predicates = new ArrayList<>();
+        List<Predicate> predicates = new ArrayList<>();
+
+        if(!StringUtils.isEmpty(cursoFilter.getNomecurso())){
+            predicates.add(builder.like(builder.lower(root.get("nomecurso")),
+                    "%" + cursoFilter.getNomecurso().toLowerCase()));
+        }
+
+        return predicates.toArray(new Predicate[predicates.size()]);
 
     }
 }
