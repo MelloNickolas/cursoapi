1c771fdc21d7cb4f59a4347e9814db243d629e9a
MelloNickolas <nickolasphmello10@gmail.com>
2023-05-25 08:27:05 -0300

    private Long Finished

--- src/main/java/br/com/etec/nickolas/cursoapi/repository/curso/CursoRepositoryImpl.java
+++ src/main/java/br/com/etec/nickolas/cursoapi/repository/curso/CursoRepositoryImpl.java
@@ -4,12 +4,11 @@ import br.com.etec.nickolas.cursoapi.model.Curso;
 import br.com.etec.nickolas.cursoapi.repository.filter.CursoFilter;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.util.Predicates;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -17,7 +16,7 @@ import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
+
 
 public class CursoRepositoryImpl implements CursoRepositoryQuery{
 
@@ -38,14 +37,30 @@ public class CursoRepositoryImpl implements CursoRepositoryQuery{
         TypedQuery<Curso> query = manager.createQuery(criteria);
         adicionarRestricoesDePagianacao(query, pageable);
 
+        return new PageImpl<>(query.getResultList(),pageable, total(cursoFilter));
+    }
 
-        return null;
+    private Long total(CursoFilter cursoFilter){
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Long> criteria = builder.createQuery(Long.class);
+        Root<Curso> root = criteria.from(Curso.class);
+
+        Predicate[] predicates = criarRestricoes(cursoFilter, builder, root);
+        criteria.where(predicates);
+        criteria.orderBy(builder.asc(root.get("nomecurso")));
+
+        criteria.select(builder.count(root));
+
+        return manager.createQuery(criteria).getSingleResult();
     }
 
     private void adicionarRestricoesDePagianacao(TypedQuery<Curso> query, Pageable pageable) {
         int paginaAtual = pageable.getPageNumber();
         int totalRegistrosPorPagina = pageable.getPageSize();
         int primeiroRegistroPágina = paginaAtual * totalRegistrosPorPagina;
+
+        query.setFirstResult(primeiroRegistroPágina);
+        query.setMaxResults(totalRegistrosPorPagina);
     }
 
     private Predicate[] criarRestricoes(CursoFilter cursoFilter, CriteriaBuilder builder, Root<Curso> root) {
