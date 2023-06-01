2dde2eaabaaa67d7c49692edd217109843ea2611
MelloNickolas <nickolasphmello10@gmail.com>
2023-06-01 07:42:30 -0300

    Extends with Query

--- src/main/java/br/com/etec/nickolas/cursoapi/repository/CidadeRepository.java
+++ src/main/java/br/com/etec/nickolas/cursoapi/repository/CidadeRepository.java
@@ -1,9 +1,10 @@
 package br.com.etec.nickolas.cursoapi.repository;
 
 import br.com.etec.nickolas.cursoapi.model.Cidade;
+import br.com.etec.nickolas.cursoapi.repository.cidade.CidadeRepositoryQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>, CidadeRepositoryQuery {
 }
