923e569f79268a4c846ff1574bf72d6865e869f6
MelloNickolas <nickolasphmello10@gmail.com>
2023-06-15 08:09:50 -0300

    Constructor...

--- src/main/java/br/com/etec/nickolas/cursoapi/repository/projections/AlunoDto.java
+++ src/main/java/br/com/etec/nickolas/cursoapi/repository/projections/AlunoDto.java
@@ -1,4 +1,19 @@
 package br.com.etec.nickolas.cursoapi.repository.projections;
 
 public class AlunoDto {
+    private Long id;
+    private String nomealuno;
+    private String nomecidade;
+    private String uf;
+    private String nomecurso;
+
+    public AlunoDto(Long id, String nomealuno, String nomecidade, String uf, String nomecurso) {
+        this.id = id;
+        this.nomealuno = nomealuno;
+        this.nomecidade = nomecidade;
+        this.uf = uf;
+        this.nomecurso = nomecurso;
+    }
+
+    
 }
